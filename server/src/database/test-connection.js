8692b4605b7eb2f9c0fc1ccb93e2734adefc063e
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-11-25 18:26:53 -0700

    Add test connection script for PostgreSQL and Redis

---
+++ server/src/database/test-connection.js
@@ -0,0 +1,43 @@
+const { testConnection: testPostgres, closePool } = require('../config/database');
+const { connectRedis, testConnection: testRedis, closeRedis } = require('../config/redis');
+
+async function testConnections() {
+
+  let allSuccess = true;
+
+
+  const pgSuccess = await testPostgres();
+  if (!pgSuccess) {
+    allSuccess = false;
+    console.log('PostgreSQL connection failed');
+  } else {
+    console.log('PostgreSQL connection successful');
+  }
+
+  try {
+    await connectRedis();
+    const redisSuccess = await testRedis();
+    if (!redisSuccess) {
+      allSuccess = false;
+      console.log('Redis connection failed');
+    } else {
+      console.log('Redis connection successful');
+    }
+  } catch (err) {
+    allSuccess = false;
+    console.log('Redis connection failed:', err.message);
+  }
+
+  if (allSuccess) {
+    console.log('All connections successful!');
+  } else {
+    console.log('Some connections failed!');
+  }
+
+  await closeRedis();
+  await closePool();
+
+  process.exit(allSuccess ? 0 : 1);
+}
+
+testConnections();
